0db185c7a46c1815eef3e39fc7ac11a01fd0d6fe
nico <nicolas.raoux@gmail.com>
2019-04-18 15:53:18 +0200

    image

--- app/src/main/java/com/example/pankathon/MainActivity.java
+++ app/src/main/java/com/example/pankathon/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.pankathon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.ibOcean).setOnClickListener(this);
+        findViewById(R.id.ibDesert).setOnClickListener(this);
+        findViewById(R.id.ibforest).setOnClickListener(this);
+        findViewById(R.id.ibCastle).setOnClickListener(this);
+        findViewById(R.id.ibSky).setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.ibOcean:
+
+                break;
+            case R.id.ibDesert:
+
+                break;
+            case R.id.ibforest:
+
+                break;
+            case R.id.ibCastle:
+
+                break;
+            case R.id.ibSky:
+
+                break;
+        }
     }
 }
